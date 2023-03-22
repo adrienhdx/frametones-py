7b54468520ca1303ec495bceba0edaeb566bc996
TheSlippyPenguin <ysos4063@gmail.com>
2023-03-22 16:03:37 +0100

    askdirectory for kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,16 +1,16 @@
 import cv2 #pip install opencv-python
-import numpy as np
 import os
 import vicosis_utils as utils
 import time
+from tkinter import filedialog
 
-video_path = r"C:\Users\adrhd\Videos\Captures\max_leon.mkv"
-out_path = r'C:\Users\adrhd\Documents\GitHub\vicosis'
+video_path = filedialog.askopenfilename(title = "Choisir un fichier", filetypes = (("Video files", "*.mp4"), ("all files", "*.*")))
 
 filename = os.path.splitext(os.path.basename(video_path))[0] # get filename without extension for saving the image file
 
 start = time.time()
 
+# mandatory parameters
 nb_frames = 1500 # 1000 frames per hour is satisfactory
 height = 128 #nb_frames // 10 # 10 frames per pixel
 K=7
@@ -19,8 +19,9 @@ cap = cv2.VideoCapture(video_path)
 
 output_image = utils.process_kmeans(source=cap, frame_count=nb_frames, output_height=height, color_count=K, logging=True, high_res=True)
 
+out_path = filedialog.askdirectory(title = "Choisir un dossier de destination", mustexist=True)
 os.chdir(out_path)
 
-cv2.imwrite(f"K_{K}_{filename}_{start-time.time()}.jpg", output_image)
+cv2.imwrite(f"K_{K}_{filename}_{time.time() - start}.jpg", output_image)
 # K_filename_time.jpg
          
